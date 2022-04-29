8299057098a2b6f1f374071f11c8e405c7b20f7f
Ryan-Elbanna <93952585+Ryan-Elbanna@users.noreply.github.com>
2022-04-29 15:26:53 -0700

    commit

---
+++ 1.js
@@ -0,0 +1,17 @@
+var mybutton = document.getElementById("scroll-to-top-button");
+   
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+// JavaScript for the Scroll To Top Button
