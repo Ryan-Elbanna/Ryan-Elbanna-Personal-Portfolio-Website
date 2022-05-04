0e2703f2d44fa49f7ed59c92a308fb8c6773fd93
Ryan-Elbanna <93952585+Ryan-Elbanna@users.noreply.github.com>
2022-05-03 21:25:49 -0700

    Update personalWeb.js

--- personalWeb.js
+++ personalWeb.js
@@ -1,17 +1,33 @@
 var mybutton = document.getElementById("scroll-to-top-button");
-   
+var headerElement = document.getElementById("header-navigation-all");
+// var sectionText = document.getElementById("first-section-page")
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     mybutton.style.display = "block";
+    // headerElement.style.borderBottom = "solid 1px white";
+    // sectionText.style.animation = "slide-up 2s"
+    // sectionText.style.opacity = "1"
+
   } else {
     mybutton.style.display = "none";
+    headerElement.style.borderBottom = "none";
   }
+
 }
 
 function topFunction() {
 window.scrollTo({top: 0, behavior: 'smooth'});
 }
 
-// JavaScript for the Scroll To Top Button
+// Header and Scroll to top functions ending
+
+var hamburgerIcon = document.querySelector(".hamburger-menu")
+var navigationBar = document.querySelector(".nav-beginning")
+
+hamburgerIcon.addEventListener("click", function() {
+  navigationBar.classList.toggle("changeState")
+})
+
