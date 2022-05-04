ad0ba28fcfac9efa93fec0d47c0eeaeaf58d669a
Ryan-Elbanna <93952585+Ryan-Elbanna@users.noreply.github.com>
2022-05-04 15:20:59 -0700

    commit

--- External-Test.js
+++ External-Test.js
@@ -24,6 +24,8 @@ window.scrollTo({top: 0, behavior: 'smooth'});
 
 // Header and Scroll to top functions ending
 
+// Hmaburger menu JS toggle function begin
+
 var hamburgerIcon = document.querySelector(".hamburger-menu")
 var navigationBar = document.querySelector(".nav-beginning")
 
@@ -31,3 +33,17 @@ hamburgerIcon.addEventListener("click", function() {
   navigationBar.classList.toggle("changeState")
 })
 
+// Hmaburger menu JS toggle function end
+
+// var form = document.getElementById("contact-mig-form-main-items");
+// var formErrorOut = document.getElementById("unique-form-error-display")
+// var formData = form.elements;
+// var formFirstName = formData["first-name"].value;
+// var formLastName = formData["last-name"].value;
+// var formEmail = formData["Email-add"].value
+
+// function formValidation() {
+//   let blank = "";
+//   if(formFirstName = "")
+//   blank += "<p> First Name Is Invalid! </p>"
+// }
